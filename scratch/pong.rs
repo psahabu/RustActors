f7b76cd1eb3b953532444fcd29a428613e761795
Preston Sahabu <sahabp@uw.edu>
2014-06-08 01:38:34 -0700

    Finally rolling out the Actor system, Cage

--- scratch/pong.rs
+++ scratch/pong.rs
@@ -1,23 +1,23 @@
-extern mod extra;
 use std::rand;
 use std::rand::Rng;
-use std::comm::SharedChan;
 use std::clone::Clone;
+use std::vec::Vec;
+use std::string::String;
 
 // rustc can derive Clone instances for you
 #[deriving(Clone)]
 struct Paddle {
-	name: ~str
+	name: String 
 }
 
 fn pong(paddles: ~[Paddle]) {
-	let mut sources = ~[];
-	let mut drains = ~[];
+	let mut sources = Vec::new();
+	let mut drains = Vec::new();
 	// instead of manually counting, use .len()
 	for _ in paddles.iter() {
-		let (drain, source): (Port<bool>, SharedChan<bool>) = SharedChan::new();
-		sources.push(source);
-		drains.push(drain);
+		let (send,recv) = channel::<bool>();
+		sources.push(send);
+		drains.push(recv);
 	}
 	// freeze the vectors now that they're built. the the compiler will stop us accidently mutating them.
 	let sources = sources;
@@ -25,14 +25,14 @@ fn pong(paddles: ~[Paddle]) {
 	let num = paddles.len(); // just do this once. it'll be copied by the spawned closures
 
 	for d in drains.move_iter() {
+		// capture variables
 		let s = sources.clone();
 		let p = paddles.clone();
 		spawn(proc() {
 			// just create one generator per task rather than one per loop iteration
-			let mut rng = rand::rng();
+			let mut rng = rand::task_rng();
 			// why not use a while loop instead of loop + if/else break?
 			while d.recv() {
-				let mut rng = rand::rng();
 				// rather than modulo, try gen_range
 				let to = rng.gen_range(0u, num+1);
 				if to == num {
@@ -43,22 +43,21 @@ fn pong(paddles: ~[Paddle]) {
 					break;
 				} else {
 					println!("{} has the puck", p[to].name);
-					s[to].send(true);
+					s.get(to).send(true);
 				}
-
 			}
 		});
 	}
 
 	// start game
-	sources[0].send(true);
+	sources.get(0).send(true);
 }
 
 fn main() {
-	let paddles = ~[
-		Paddle {name: ~"Albert"},
-		Paddle {name: ~"Bertha"},
-		Paddle {name: ~"Carol"}
+	let paddles = box [
+		Paddle {name: "Albert".to_string()},
+		Paddle {name: "Bertha".to_string()},
+		Paddle {name: "Carol".to_string()}
 	];
 	pong(paddles);
 }
